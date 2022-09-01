3149f58427b344c66ff2653956e42e243d9662c7
Siedler87 <112324174+Siedler87@users.noreply.github.com>
2022-09-02 00:51:52 +0200

    Initial commit

---
+++ config.js
@@ -0,0 +1,11 @@
+const config = {
+    db: {
+      /* don't expose password or any sensitive info, done only for demo */
+      host: "mysql31.1blu.de",
+      user: "s155714_3383892",
+      password: "!qay2wsx3EDC",
+      database: "db155714x3383892",
+    },
+    listPerPage: 10,
+  };
+  module.exports = config;
