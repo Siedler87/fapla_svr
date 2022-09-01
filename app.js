f3900083d404f101ee5b15af49665d7a190f057c
Siedler87 <112324174+Siedler87@users.noreply.github.com>
2022-09-02 01:29:59 +0200

    Delete app.js

--- app.js
+++
@@ -1,7 +0,0 @@
-{
-  "name": "Fapla API",
-  "description": "Fapla",
-  "repository": "https://github.com/Siedler87/fapla",
-  "logo": "https://fapla.herokuapp.com/logo.png",
-  "keywords": ["node", "express", "static"]
-}
