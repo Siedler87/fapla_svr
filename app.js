b5a129765a30d6d3c9af48d86c560c69c7b5471c
Siedler87 <112324174+Siedler87@users.noreply.github.com>
2022-09-02 00:57:41 +0200

    1
    1

---
+++ app.js
@@ -0,0 +1,7 @@
+{
+  "name": "Fapla API",
+  "description": "Fapla",
+  "repository": "https://github.com/Siedler87/fapla",
+  "logo": "https://fapla.herokuapp.com/logo.png",
+  "keywords": ["node", "express", "static"]
+}
