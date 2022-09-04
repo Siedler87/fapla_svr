283b2495e36d24d7911c2a734d3042b8d9089078
Siedler87 <112324174+Siedler87@users.noreply.github.com>
2022-09-05 01:15:26 +0200

    Update fapla.js

--- services/fapla.js
+++ services/fapla.js
@@ -18,16 +18,17 @@ async function getMultiple(page = 1){
   }
 }
 
-async function create(user){
+async function create(fapla){
   const result = await db.query(
     `INSERT INTO user 
     (accountname, vorname, familienname, 
      mail, gebdatum, letzterlogin, 
      registrierungsdatum, passwort, usertype) 
     VALUES (
-      ${fapla.user_accountname}, ${fapla.user_vorname}, ${fapla.user_familienname}, 
-      ${fapla.user_mail}, ${fapla.user_gebdatum}, ${fapla.user_gebdatum},
-      ${fapla.user_registrierungsdatum}, ${fapla.user_passwort}, ${fapla.user_usertype})`
+      ${fapla.accountname}, ${fapla.vorname}, ${fapla.familienname}, 
+      ${fapla.mail}, ${fapla.gebdatum}, ${fapla.gebdatum},
+      ${fapla.registrierungsdatum}, ${fapla.passwort}, ${fapla.usertype}
+      )`
   );
 
   let message = 'Error in creating user entry';
