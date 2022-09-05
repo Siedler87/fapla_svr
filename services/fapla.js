c945afbf2e66804a8488a10a2355d94491213476
Siedler87 <112324174+Siedler87@users.noreply.github.com>
2022-09-06 01:03:59 +0200

    update
    update

--- services/fapla.js
+++ services/fapla.js
@@ -19,12 +19,10 @@ async function getMultiple(page = 1){
 }
 
 async function create(fapla){
+  
   const result = await db.query(
-    `INSERT INTO user 
-    (accountname, vorname, familienname, 
-     mail, gebdatum, letzterlogin, 
-     registrierungsdatum, passwort, usertype) 
-    VALUES (
+    `INSERT INTO user (accountname, vorname, familienname, mail, gebdatum, letzterlogin, registrierungsdatum, passwort, usertype) 
+      VALUES (
       ${fapla.accountname}, ${fapla.vorname}, ${fapla.familienname}, 
       ${fapla.mail}, ${fapla.gebdatum}, ${fapla.gebdatum},
       ${fapla.registrierungsdatum}, ${fapla.passwort}, ${fapla.usertype}
