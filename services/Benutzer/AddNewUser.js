b11ee697ac7b17e801d049c7c95910c60dc87b7d
Siedler87 <112324174+Siedler87@users.noreply.github.com>
2022-09-19 00:30:44 +0200

    time
    time

--- services/Benutzer/AddNewUser.js
+++ services/Benutzer/AddNewUser.js
@@ -1,18 +1,16 @@
 const db = require('../db');
-const helper = require('../../helper');
-const config = require('../../config');
-
-// current date
-let date_ob = new Date();
-let date = ("0" + date_ob.getDate()).slice(-2);
-let month = ("0" + (date_ob.getMonth() + 1)).slice(-2);
-let year = date_ob.getFullYear();
-let hours = date_ob.getHours();
-let minutes = date_ob.getMinutes();
-let seconds = date_ob.getSeconds();
-var CurrentTimestamp = "'"+year + "-" + month + "-" + date + " " + hours + ":" + minutes + ":" + seconds+"'";
 
 async function create(AddNewUser){
+
+  // current date
+  let date_ob = new Date();
+  let date = ("0" + date_ob.getDate()).slice(-2);
+  let month = ("0" + (date_ob.getMonth() + 1)).slice(-2);
+  let year = date_ob.getFullYear();
+  let hours = date_ob.getHours();
+  let minutes = date_ob.getMinutes();
+  let seconds = date_ob.getSeconds();
+  var CurrentTimestamp = "'"+year + "-" + month + "-" + date + " " + hours + ":" + minutes + ":" + seconds+"'";
 
   var sql = 'INSERT INTO Benutzer (`Accountname`, `Vorname`, `Nachname`, `Geburtsdatum`, `Email`, `Passwort`, `LetzterLogin`, `Hintergrundfarbe`) VALUES (?, ?, ?, ?, ?, ?, ?, ? )';
   const result = await db.query(sql, [AddNewUser.accountname.trim(), AddNewUser.vorname.trim(), AddNewUser.nachname.trim(), AddNewUser.geburtsdatum.trim(), AddNewUser.email.trim(), AddNewUser.passwort.trim(), CurrentTimestamp, AddNewUser.hintergrundfarbe.trim()], function (err, result) {
