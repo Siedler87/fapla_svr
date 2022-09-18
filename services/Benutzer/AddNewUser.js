c29151b7ddb884b5f007ba9bda26b6f4ea273105
Siedler87 <112324174+Siedler87@users.noreply.github.com>
2022-09-19 00:10:49 +0200

    Update AddNewUser.js
    Update

--- services/Benutzer/AddNewUser.js
+++ services/Benutzer/AddNewUser.js
@@ -10,24 +10,23 @@ let year = date_ob.getFullYear();
 let hours = date_ob.getHours();
 let minutes = date_ob.getMinutes();
 let seconds = date_ob.getSeconds();
-var CURRENT_TIMESTAMP = "'"+year + "-" + month + "-" + date + " " + hours + ":" + minutes + ":" + seconds+"'";
+var CurrentTimestamp = "'"+year + "-" + month + "-" + date + " " + hours + ":" + minutes + ":" + seconds+"'";
 
 async function create(AddNewUser){
 
-  const result = await db.query(
-    `INSERT INTO Benutzer (Accountname, Vorname, Nachname, Geburtsdatum, Email, Passwort, LetzterLogin, Hintergrundfarbe)
-    VALUES (
-      '${AddNewUser.accountname}','${AddNewUser.vorname}','${AddNewUser.nachname}','${AddNewUser.geburtsdatum}','${AddNewUser.email}',
-      '${AddNewUser.passwort}', '${CURRENT_TIMESTAMP}', '${AddNewUser.hintergrundfarbe})'`
-  );
-
-  let message = 'Error in creating user entry';
-
+  var sql = 'INSERT INTO Benutzer (`Accountname`, `Vorname`, `Nachname`, `Geburtsdatum`, `Email`, `Passwort`, `LetzterLogin`, `Hintergrundfarbe`) VALUES (?, ?, ?, ?, ?, ?, ?, ? )';
+  const result = await db.query(sql, [AddNewUser.Accountname, AddNewUser.Vorname, AddNewUser.Nachname, AddNewUser.Geburtsdatum, AddNewUser.Email, AddNewUser.Passwort, CurrentTimestamp, AddNewUser.Hintergrundfarbe], function (err, result) {
+    if (err) {
+      let message = 'Error';
+      return {message};
+    };
+  
+  });
   if (result.affectedRows) {
-    message = 'User created successfully';
+    message = 'Success';
   }
-
   return {message};
+
 }
 
 module.exports = {
