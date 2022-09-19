01d8d70e01afba9063fc442133d450a0d12df571
Siedler87 <112324174+Siedler87@users.noreply.github.com>
2022-09-19 22:07:09 +0200

    time
    time

--- services/Benutzer/AddNewUser.js
+++ services/Benutzer/AddNewUser.js
@@ -7,9 +7,9 @@ async function create(AddNewUser){
   let date = ("0" + date_ob.getDate()).slice(-2);
   let month = ("0" + (date_ob.getMonth() + 1)).slice(-2);
   let year = date_ob.getFullYear();
-  let hours = date_ob.getHours();
-  let minutes = date_ob.getMinutes();
-  let seconds = date_ob.getSeconds();
+  let hours = ("0" + (date_ob.getHours + 1)).slice(-2);
+  let minutes = ("0" + (date_ob.getMinutes() + 1)).slice(-2);
+  let seconds = ("0" + (date_ob.getSeconds() + 1)).slice(-2);
   var CurrentTimestamp = "'"+year + "-" + month + "-" + date + " " + hours + ":" + minutes + ":" + seconds+"'";
 
   console.log(CurrentTimestamp);
