596060143d4fe22a5045ae562987b26a465315bc
Siedler87 <112324174+Siedler87@users.noreply.github.com>
2022-09-21 22:49:47 +0200

    Update AddNewFamily.js
    update

--- services/Benutzer/AddNewFamily.js
+++ services/Benutzer/AddNewFamily.js
@@ -4,8 +4,8 @@ async function create(AddNewFamily){
 
   // current date
   
-  var sql = 'INSERT INTO Familien(`Familienname`, `Passwort`) VALUES (?,?)';
-  const result = await db.query(sql, [AddNewFamily.familienname.trim(), AddNewFamily.passwort.trim()], function (err, result) {
+  var sql = 'INSERT INTO Familien(`Accountname`,`Familienname`, `Passwort`) VALUES (?,?)';
+  const result = await db.query(sql, [AddNewFamily.accountname.trim(), AddNewFamily.familienname.trim(), AddNewFamily.passwort.trim()], function (err, result) {
     if (err) {
       let message = 'Error';
       return {message};
