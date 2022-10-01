8f2b29a86cbcc18e012a1011f57ef093f05a322e
Siedler87 <112324174+Siedler87@users.noreply.github.com>
2022-10-01 18:34:44 +0200

    Update GetLoginUser.js

--- services/Benutzer/GetLoginUser.js
+++ services/Benutzer/GetLoginUser.js
@@ -3,10 +3,12 @@ const db = require('../db');
 async function GetLoginUser(GetLoginUser){
 
   // current date
+
+  console.log("Objekt: "+GetLoginUser);
   console.log("Accountname: "+GetLoginUser.accountname.trim());
   console.log("Passwort: "+GetLoginUser.passwort.trim());
   let CurrentTimestamp = new Date().toISOString().slice(0, 19).replace('T', ' ');
-  var sql = 'SELECT `Passwort` FROM `Benutzer` WHERE `Accountname` = ?;';
+  var sql = 'SELECT `Passwort` FROM `Benutzer` WHERE `Accountname` = "?";';
   const result = await db.query(sql, [GetLoginUser.accountname.trim()], function (err, result) {
     if (err) {
       console.log(err);
@@ -16,7 +18,7 @@ async function GetLoginUser(GetLoginUser){
   });
   console.log("Passwort in DB: "+result[0].Passwort);
   if (result[0].Passwort == GetLoginUser.passwort.trim()){
-      var sql = 'UPDATE `Benutzer` SET `LetzterLogin`=? WHERE `Accountname`=?';
+      var sql = 'UPDATE `Benutzer` SET `LetzterLogin`=? WHERE `Accountname`="?"';
       const subresult = await db.query(sql, [CurrentTimestamp, GetLoginUser.accountname.trim()], function (err, result) {
         if (err) {
           let message = 'Error';
