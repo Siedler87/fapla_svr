1a7cef53f0c9eb08fed606d8ffd3e02c54e8c586
Siedler87 <112324174+Siedler87@users.noreply.github.com>
2022-10-01 20:03:50 +0200

    Update GetLoginUser.js

--- services/Benutzer/GetLoginUser.js
+++ services/Benutzer/GetLoginUser.js
@@ -10,7 +10,7 @@ async function GetLoginUser(GetLoginUser){
   let CurrentTimestamp = new Date().toISOString().slice(0, 19).replace('T', ' ');
   var sql = 'SELECT `Passwort` FROM `Benutzer` WHERE `Accountname` =  "?";';
   console.log(sql+GetLoginUser.accountname.trim());
-  const ergebnis = await db.query(sql, [GetLoginUser.accountname.trim()], async function (err, result) {
+  const ergebnis = await db.query(sql, [GetLoginUser.accountname.trim()], function (err, result) {
     if (err) {
       console.log(err);
       let message = 'Error';
@@ -27,17 +27,20 @@ async function GetLoginUser(GetLoginUser){
       console.log("Passwort in DB: "+ergebnis[0].Passwort);
       if (ergebnis[0].Passwort == GetLoginUser.passwort.trim()){
           var sql = 'UPDATE `Benutzer` SET `LetzterLogin`=? WHERE `Accountname`="?"';
-          const subresult = await db.query(sql, [CurrentTimestamp, GetLoginUser.accountname.trim()], function (err, result) {
+          const subresult = db.query(sql, [CurrentTimestamp, GetLoginUser.accountname.trim()], function (err, result) {
             if (err) {
               let message = 'Error';
-              
+              console.log(message);
               return {message};
             };
+            let message = 'Login_success';
+            console.log(message);
+            return {message};
           });
-          let message = 'Login_success';
-          return {message};
+
       } else {
         let message = 'Password_wrong';
+        console.log(message);
         return {message};
       }
     }
