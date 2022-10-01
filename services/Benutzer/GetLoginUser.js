7728706a5fba3e3055387ceb08642f470dea828c
Siedler87 <112324174+Siedler87@users.noreply.github.com>
2022-10-01 18:41:29 +0200

    Update GetLoginUser.js

--- services/Benutzer/GetLoginUser.js
+++ services/Benutzer/GetLoginUser.js
@@ -17,25 +17,29 @@ async function GetLoginUser(GetLoginUser){
     };
   });
   if (result == null) {
+    console.log("Account nicht gefunden");
     let message = 'Account_not_found';
     return {message};
-  }
-  console.log("Passwort in DB: "+result[0].Passwort);
-  if (result[0].Passwort == GetLoginUser.passwort.trim()){
-      var sql = 'UPDATE `Benutzer` SET `LetzterLogin`=? WHERE `Accountname`="?"';
-      const subresult = await db.query(sql, [CurrentTimestamp, GetLoginUser.accountname.trim()], function (err, result) {
-        if (err) {
-          let message = 'Error';
-          
-          return {message};
-        };
-      });
-      let message = 'Login_success';
-      return {message};
   } else {
-    let message = 'Password_wrong';
-    return {message};
+
+    console.log("Passwort in DB: "+result[0].Passwort);
+    if (result[0].Passwort == GetLoginUser.passwort.trim()){
+        var sql = 'UPDATE `Benutzer` SET `LetzterLogin`=? WHERE `Accountname`="?"';
+        const subresult = await db.query(sql, [CurrentTimestamp, GetLoginUser.accountname.trim()], function (err, result) {
+          if (err) {
+            let message = 'Error';
+            
+            return {message};
+          };
+        });
+        let message = 'Login_success';
+        return {message};
+    } else {
+      let message = 'Password_wrong';
+      return {message};
+    }
   }
+  
 }
 
 module.exports = {
