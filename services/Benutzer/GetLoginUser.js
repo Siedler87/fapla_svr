dab1c5005e95ffb2ffbb4cbf2f8fea418cb08174
Siedler87 <112324174+Siedler87@users.noreply.github.com>
2022-10-01 19:53:32 +0200

    Update GetLoginUser.js

--- services/Benutzer/GetLoginUser.js
+++ services/Benutzer/GetLoginUser.js
@@ -8,26 +8,26 @@ async function GetLoginUser(GetLoginUser){
   console.log("Accountname: "+GetLoginUser.accountname.trim());
   console.log("Passwort: "+GetLoginUser.passwort.trim());
   let CurrentTimestamp = new Date().toISOString().slice(0, 19).replace('T', ' ');
-  var sql = 'SELECT Passwort FROM Benutzer WHERE Accountname =  "?";';
+  var sql = 'SELECT `Passwort` FROM `Benutzer` WHERE `Accountname` =  ?;';
   console.log(sql+GetLoginUser.accountname.trim());
-  const result = await db.query(sql, [GetLoginUser.accountname.trim()], async function (err, result) {
+  const ergebnis = await db.query(sql, [GetLoginUser.accountname.trim()], function (err, result) {
     if (err) {
       console.log(err);
       let message = 'Error';
       return {message};
     };
-    console.log("Ergebnis: "+result);
-    console.log("Ergebnis: "+result[0]);
-    if (result == null) {
+    console.log("Ergebnis: "+ergebnis);
+    console.log("Ergebnis: "+ergebnis[0]);
+    if (ergebnis == null) {
       console.log("Account nicht gefunden");
       let message = 'Account_not_found';
       return {message};
     } else {
   
-      console.log("Passwort in DB: "+result[0].Passwort);
-      if (result[0].Passwort == GetLoginUser.passwort.trim()){
-          var sql = 'UPDATE Benutzer SET LetzterLogin=? WHERE Accountname="?"';
-          const subresult = await db.query(sql, [CurrentTimestamp, GetLoginUser.accountname.trim()], function (err, result) {
+      console.log("Passwort in DB: "+ergebnis[0].Passwort);
+      if (ergebnis[0].Passwort == GetLoginUser.passwort.trim()){
+          var sql = 'UPDATE `Benutzer` SET `LetzterLogin`=? WHERE `Accountname`=?';
+          const subresult = db.query(sql, [CurrentTimestamp, GetLoginUser.accountname.trim()], function (err, result) {
             if (err) {
               let message = 'Error';
               
@@ -41,7 +41,7 @@ async function GetLoginUser(GetLoginUser){
         return {message};
       }
     }
-
+    
   });
   
   
