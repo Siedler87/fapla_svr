03836828ce5f7539ead968add9a5a1a28ff6fbad
Siedler87 <112324174+Siedler87@users.noreply.github.com>
2022-10-01 23:27:39 +0200

    Update GetLoginFamily.js

--- services/Familien/GetLoginFamily.js
+++ services/Familien/GetLoginFamily.js
@@ -3,18 +3,23 @@ const db = require('../db');
 async function GetLoginFamily(GetLoginFamily){
 
   var sql = 'SELECT `Passwort` FROM `Familien` WHERE `Accountname` = ?;';
-  const result = await db.query(sql, [GetLoginFamily.accountname.trim()], function (err, result) {
+  const ergebnis = await db.query(sql, [GetLoginFamily.accountname.trim()], function (err, result) {
     if (err) {
       let message = 'Error';
       return {message};
     };
   });
-  if (result[0].Passwort == GetLoginFamily.passwort.trim()){
-      let message = 'Login_success';
-      return {message};
-  } else {
-    let message = 'Password_wrong';
+  if (!ergebnis) {
+    let message = 'Account_not_found';
     return {message};
+  } else {
+    if (ergebnis[0].Passwort == GetLoginFamily.passwort.trim()){
+        let message = 'Login_success';
+        return {message};
+    } else {
+      let message = 'Password_wrong';
+      return {message};
+    }
   }
 }
 
