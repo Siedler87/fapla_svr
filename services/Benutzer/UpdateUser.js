1be57e4e5a5106c5f0f36815e5f7ad1de56279da
Siedler87 <112324174+Siedler87@users.noreply.github.com>
2022-10-03 21:13:05 +0200

    Update UpdateUser.js

--- services/Benutzer/UpdateUser.js
+++ services/Benutzer/UpdateUser.js
@@ -2,16 +2,18 @@ const db = require('../db');
 
 async function UpdateUser(UpdateUser){
 
-  if (UpdateUser.benid != null && UpdateUser.nachname != null && UpdateUser.vorname  != null && UpdateUser.geburtsdatum != null && UpdateUser.email != null) {
+  // Hauptbenutzer aktualisieren
+  // Vorname, Nachname, Geburtsdatum, Email
+  if (UpdateUser.benid != null && UpdateUser.vorname != null && UpdateUser.nachname != null && UpdateUser.geburtsdatum != null && UpdateUser.email != null) {
     var sql = 'UPDATE `Benutzer` SET `Vorname` = ?, `Nachname` = ? , `Geburtsdatum` = ?, `Email` = ?  WHERE `BenID`= ?) VALUES (?, ?, ?, ?, ?);';
-    const ergebnis = await db.query(sql, [UpdateUser.vorname.trim(), UpdateUser.nachname.trim(), UpdateUser.geburtsdatum.trim(), UpdateUser.email.trim(), UpdateUser.hintergrundfarbe.trim(), UpdateUser.benid], function (err, result) {
+    const ergebnis = await db.query(sql, [UpdateUser.vorname.trim(), UpdateUser.nachname.trim(), UpdateUser.geburtsdatum.trim(), UpdateUser.email.trim(), UpdateUser.benid], function (err, result) {
       if (err) {
         let message = 'Error';
         return {message};
       };
     });
     if (ergebnis.affectedRows) {
-      var sql = 'SELECT `Accountname`, `Vorname`, `Nachname`, `Geburtsdatum`, `Email`, `Passwort`, `Hintergrundfarbe`, `BildID` FROM `Benutzer` WHERE `BenID`= ?;';
+      var sql = 'SELECT `BenID`,`Accountname`, `Vorname`, `Nachname`, `Geburtsdatum`, `Email`, `Passwort`, `Hintergrundfarbe`, `BildID` FROM `Benutzer` WHERE `BenID`= ?;';
       const subergebnis = await db.query(sql, [UpdateUser.benid], function (err, result) {
         if (err) {
           let message = 'Error';
@@ -20,13 +22,14 @@ async function UpdateUser(UpdateUser){
       });
       let message = 'Update_success';
       let benid = subergebnis[0].BenID;
+      let accountname = subergebnis[0].accountname;
       let vorname = subergebnis[0].Vorname;
       let nachname = subergebnis[0].Nachname;
       let geburtsdatum = subergebnis[0].Geburtsdatum;
       let email = subergebnis[0].Email;
       let hintergrundfarbe = subergebnis[0].Hintergrundfarbe;
       let bildid = subergebnis[0].BildID;
-      return {message, benid, vorname, nachname, geburtsdatum, email, hintergrundfarbe, bildid};
+      return {message, benid, accountname, vorname, nachname, geburtsdatum, email, hintergrundfarbe, bildid};
     }  
   } 
   if (UpdateUser.benid != null && UpdateUser.passwort != null){
@@ -60,12 +63,42 @@ async function UpdateUser(UpdateUser){
               }
           }  
         });
-      } 
-      
-      if ((UpdateUser.benid == null ) || (UpdateUser.nachname == null && UpdateUser.vorname  == null && UpdateUser.geburtsdatum == null && UpdateUser.email == null && UpdateUser.passwort == null)){
-        let message = 'Keine_Parameter';
+  } 
+  // Nebenbenutzer aktualisieren
+  // Vorname, Nachname, Geburtsdatum - keine Email!
+  if (UpdateUser.benid != null && UpdateUser.vorname != null && UpdateUser.nachname != null && UpdateUser.geburtsdatum != null && UpdateUser.email == null) {
+    var sql = 'UPDATE `Benutzer` SET `Vorname` = ?, `Nachname` = ? , `Geburtsdatum` = ? WHERE `BenID`= ?) VALUES (?, ?, ?, ?, ?);';
+    const ergebnis = await db.query(sql, [UpdateUser.vorname.trim(), UpdateUser.nachname.trim(), UpdateUser.geburtsdatum.trim(), UpdateUser.benid], function (err, result) {
+      if (err) {
+        let message = 'Error';
         return {message};
-    }
+      };
+    });
+    if (ergebnis.affectedRows) {
+      var sql = 'SELECT `BenID`, `Accountname`, `Vorname`, `Nachname`, `Geburtsdatum`, `Hintergrundfarbe`, `BildID` FROM `Benutzer` WHERE `BenID`= ?;';
+      const subergebnis = await db.query(sql, [UpdateUser.benid], function (err, result) {
+        if (err) {
+          let message = 'Error';
+          return {message};
+        };
+      });
+      let message = 'Update_success';
+      let benid = subergebnis[0].BenID;
+      let accountname = subergebnis[0].accountname;
+      let vorname = subergebnis[0].Vorname;
+      let nachname = subergebnis[0].Nachname;
+      let geburtsdatum = subergebnis[0].Geburtsdatum;
+      let hintergrundfarbe = subergebnis[0].Hintergrundfarbe;
+      let bildid = subergebnis[0].BildID;
+      return {message, benid, accountname, vorname, nachname, geburtsdatum, hintergrundfarbe, bildid};
+    }  
+  }
+
+  // bei falschen Parameten
+  if ((UpdateUser.benid == null ) || (UpdateUser.nachname == null && UpdateUser.vorname  == null && UpdateUser.geburtsdatum == null && UpdateUser.email == null && UpdateUser.passwort == null)){
+    let message = 'Keine_Parameter';
+    return {message};
+  }
 
   }
 
