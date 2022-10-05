797c3db4ff7335d049a793d329fa79c9fbf1210d
Siedler87 <112324174+Siedler87@users.noreply.github.com>
2022-10-05 16:50:50 +0200

    Update AddNewUser.js

--- services/Benutzer/AddNewUser.js
+++ services/Benutzer/AddNewUser.js
@@ -5,46 +5,35 @@ async function AddNewUser(AddNewUser){
   // current date
   let CurrentTimestamp = new Date().toISOString().slice(0, 19).replace('T', ' ');
 
-  if (AddNewUser.email != null && AddNewUser.passwort != null){
+  if (AddNewUser.email != null && AddNewUser.passwort != null && AddNewUser.email != undefined && AddNewUser.passwort != undefined){
     var sql = 'INSERT INTO Benutzer (`Accountname`, `Vorname`, `Nachname`, `Geburtsdatum`, `Email`, `Passwort`, `LetzterLogin`, `Hintergrundfarbe`) VALUES (?, ?, ?, ?, ?, ?, ?, ? )';
-    const result = await db.query(sql, [AddNewUser.accountname.trim(), AddNewUser.vorname.trim(), AddNewUser.nachname.trim(), AddNewUser.geburtsdatum.trim(), AddNewUser.email.trim(), AddNewUser.passwort.trim(), CurrentTimestamp, AddNewUser.hintergrundfarbe.trim()], function (err, result) {
+    const ergebnishaupt = await db.query(sql, [AddNewUser.accountname.trim(), AddNewUser.vorname.trim(), AddNewUser.nachname.trim(), AddNewUser.geburtsdatum.trim(), AddNewUser.email.trim(), AddNewUser.passwort.trim(), CurrentTimestamp, AddNewUser.hintergrundfarbe.trim()], function (err, result) {
       if (err) {
         let message = 'Error';
         return {message};
       };
-    
     });
-    if (result.affectedRows) {
-      var sql = 'SELECT `BenID` FROM `Benutzer` WHERE `Accountname` = ?';
-      const result = await db.query(sql, [AddNewUser.accountname.trim()], function (err, result) {
-        if (err) {
-          let message = 'Error';
-          return {message};
-        };
-      });
-      return (result[0].BenID);
-    }
   } else {
     var sql = 'INSERT INTO Benutzer (`Accountname`, `Vorname`, `Nachname`, `Geburtsdatum`, `Hintergrundfarbe`) VALUES (?, ?, ?, ?, ? )';
-    const result = await db.query(sql, [AddNewUser.accountname.trim(), AddNewUser.vorname.trim(), AddNewUser.nachname.trim(), AddNewUser.geburtsdatum.trim(), AddNewUser.hintergrundfarbe.trim()], function (err, result) {
+    const ergebnisneben = await db.query(sql, [AddNewUser.accountname.trim(), AddNewUser.vorname.trim(), AddNewUser.nachname.trim(), AddNewUser.geburtsdatum.trim(), AddNewUser.hintergrundfarbe.trim()], function (err, result) {
       if (err) {
         let message = 'Error';
         return {message};
       };
     });
-  
-  if (result.affectedRows) {
-    var sql = 'SELECT `BenID` FROM `Benutzer` WHERE `Accountname` = ?';
-    const result = await db.query(sql, [AddNewUser.accountname.trim()], function (err, result) {
-      if (err) {
-        let message = 'Error';
-        return {message};
-      };
-    });
-    return (result[0].BenID);
   }
+  if (ergebnishaupt.affectedRows || ergebnisneben.affectedRows) {
+    var sql = 'SELECT `BenID` FROM `Benutzer` WHERE `Accountname` = ?';
+    const ergebnis = await db.query(sql, [AddNewUser.accountname.trim()], function (err, result) {
+      if (err) {
+        let message = 'Error';
+        return {message};
+      };
+    });
+    return (ergebnis[0].BenID);
   }
 }
+
 
 module.exports = {
   AddNewUser
