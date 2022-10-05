b8ebb234a82138a03342115faeb0a84847a3e867
Siedler87 <112324174+Siedler87@users.noreply.github.com>
2022-10-05 16:21:54 +0200

    Update AddNewUser.js

--- services/Benutzer/AddNewUser.js
+++ services/Benutzer/AddNewUser.js
@@ -5,14 +5,26 @@ async function AddNewUser(AddNewUser){
   // current date
   let CurrentTimestamp = new Date().toISOString().slice(0, 19).replace('T', ' ');
 
-  var sql = 'INSERT INTO Benutzer (`Accountname`, `Vorname`, `Nachname`, `Geburtsdatum`, `Email`, `Passwort`, `LetzterLogin`, `Hintergrundfarbe`) VALUES (?, ?, ?, ?, ?, ?, ?, ? )';
-  const result = await db.query(sql, [AddNewUser.accountname.trim(), AddNewUser.vorname.trim(), AddNewUser.nachname.trim(), AddNewUser.geburtsdatum.trim(), AddNewUser.email.trim(), AddNewUser.passwort.trim(), CurrentTimestamp, AddNewUser.hintergrundfarbe.trim()], function (err, result) {
-    if (err) {
-      let message = 'Error';
-      return {message};
-    };
-  
-  });
+  if (AddNewUser.email != null && AddNewUser.passwort != null){
+    var sql = 'INSERT INTO Benutzer (`Accountname`, `Vorname`, `Nachname`, `Geburtsdatum`, `Email`, `Passwort`, `LetzterLogin`, `Hintergrundfarbe`) VALUES (?, ?, ?, ?, ?, ?, ?, ? )';
+    const result = await db.query(sql, [AddNewUser.accountname.trim(), AddNewUser.vorname.trim(), AddNewUser.nachname.trim(), AddNewUser.geburtsdatum.trim(), AddNewUser.email.trim(), AddNewUser.passwort.trim(), CurrentTimestamp, AddNewUser.hintergrundfarbe.trim()], function (err, result) {
+      if (err) {
+        let message = 'Error';
+        return {message};
+      };
+    
+    });
+  } else {
+    var sql = 'INSERT INTO Benutzer (`Accountname`, `Vorname`, `Nachname`, `Geburtsdatum`, `Hintergrundfarbe`) VALUES (?, ?, ?, ?, ? )';
+    const result = await db.query(sql, [AddNewUser.accountname.trim(), AddNewUser.vorname.trim(), AddNewUser.nachname.trim(), AddNewUser.geburtsdatum.trim(), AddNewUser.hintergrundfarbe.trim()], function (err, result) {
+      if (err) {
+        let message = 'Error';
+        return {message};
+      };
+    
+    });
+  }
+
   if (result.affectedRows) {
     var sql = 'SELECT `BenID` FROM `Benutzer` WHERE `Accountname` = ?';
     const result = await db.query(sql, [AddNewUser.accountname.trim()], function (err, result) {
