96b9d38b185c6a1c61977598cc2f1c635913af6a
Siedler87 <112324174+Siedler87@users.noreply.github.com>
2022-10-05 02:55:43 +0200

    Update DeleteUser.js

--- services/Benutzer/DeleteUser.js
+++ services/Benutzer/DeleteUser.js
@@ -2,36 +2,84 @@ const db = require('../db');
 
 async function DeleteUser(DeleteUser){
 
+  if (DeleteUser.benid != null && DeleteUser.passwort != null && DeleteUser.aktion === 'hauptaccountloeschen'){
+    var sql = 'SELECT `Passwort` FROM `Benutzer` WHERE `BenID` =  ?';
+    const ergebnis = await db.query(sql, [DeleteUser.benid], async function (err, result) {
+      if (err) {
+        console.log(err);
+        let message = 'Error';
+        return {message};
+      };
+    })
+      if (ergebnis.length < 1)  {
+        let message = 'Account_not_found';
+        return {message};
+      } else {
+        if (ergebnis[0].Passwort == DeleteUser.passwort.trim()){
 
-  //ToDo: Prüfen, ob BenID woanders genutzt wird (Zuordungstabellen, dann dort zuerst löschen)
-  var sql = 'DELETE FROM `Benutzer` WHERE `BenID`= ?';
-  const ergebnis = await db.query(sql, [DeleteUser.benid], function (err, result) {
-    if (err) {
-      let message = 'Error';
-      return {message};
-    };
-  });
-  if (ergebnis.affectedRows) {
-    var sql = 'SELECT 1 FROM `Benutzer` WHERE `BenID`= ?';
-    const subergebnis = await db.query(sql, [DeleteUser.benid], function (err, result) {
+            //ToDo: Prüfen, ob BenID woanders genutzt wird (Zuordungstabellen, dann dort zuerst löschen)
+            var sql = 'DELETE FROM `Benutzer` WHERE `BenID`= ?';
+            const ergebnis = await db.query(sql, [DeleteUser.benid], function (err, result) {
+              if (err) {
+                let message = 'Error';
+                return {message};
+              };
+            });
+            if (ergebnis.affectedRows) {
+              var sql = 'SELECT 1 FROM `Benutzer` WHERE `BenID`= ?';
+              const subergebnis = await db.query(sql, [DeleteUser.benid], function (err, result) {
+                if (err) {
+                  let message = 'Error';
+                  return {message};
+                };
+              });
+              if (subergebnis.length < 1)  {
+                let message = 'Delete_success';
+                return {message};
+              } else {
+                let message = 'Delete_fail';
+                return {message};
+              }
+            } else {
+              let message = 'Account_not_found';
+              return {message};
+            }  
+        } 
+      }
+
+  } 
+  if (DeleteUser.benid != null && DeleteUser.aktion === 'nebenaccountloeschen'){
+    
+    //ToDo: Prüfen, ob BenID woanders genutzt wird (Zuordungstabellen, dann dort zuerst löschen)
+    var sql = 'DELETE FROM `Benutzer` WHERE `BenID`= ?';
+    const ergebnis = await db.query(sql, [DeleteUser.benid], function (err, result) {
       if (err) {
         let message = 'Error';
         return {message};
       };
     });
-    if (subergebnis.length < 1)  {
-      let message = 'Delete_success';
-      return {message};
+    if (ergebnis.affectedRows) {
+      var sql = 'SELECT 1 FROM `Benutzer` WHERE `BenID`= ?';
+      const subergebnis = await db.query(sql, [DeleteUser.benid], function (err, result) {
+        if (err) {
+          let message = 'Error';
+          return {message};
+        };
+      });
+      if (subergebnis.length < 1)  {
+        let message = 'Delete_success';
+        return {message};
+      } else {
+        let message = 'Delete_fail';
+        return {message};
+      }
     } else {
-      let message = 'Delete_fail';
+      let message = 'Account_not_found';
       return {message};
-    }
-  } else {
-    let message = 'Account_not_found';
-    return {message};
-  }  
-}
+    } 
+  }
 
+}
 module.exports = {
   DeleteUser
 }
