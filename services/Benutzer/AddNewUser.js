f3dbed756bc95865cdeb2100f280f8368325584b
Siedler87 <112324174+Siedler87@users.noreply.github.com>
2022-10-05 16:41:25 +0200

    Update AddNewUser.js

--- services/Benutzer/AddNewUser.js
+++ services/Benutzer/AddNewUser.js
@@ -5,7 +5,7 @@ async function AddNewUser(AddNewUser){
   // current date
   let CurrentTimestamp = new Date().toISOString().slice(0, 19).replace('T', ' ');
 
-  if (AddNewUser.email != null && AddNewUser.passwort != null){
+  if (AddNewUser.email != undefined && AddNewUser.passwort != undefined){
     var sql = 'INSERT INTO Benutzer (`Accountname`, `Vorname`, `Nachname`, `Geburtsdatum`, `Email`, `Passwort`, `LetzterLogin`, `Hintergrundfarbe`) VALUES (?, ?, ?, ?, ?, ?, ?, ? )';
     const result = await db.query(sql, [AddNewUser.accountname.trim(), AddNewUser.vorname.trim(), AddNewUser.nachname.trim(), AddNewUser.geburtsdatum.trim(), AddNewUser.email.trim(), AddNewUser.passwort.trim(), CurrentTimestamp, AddNewUser.hintergrundfarbe.trim()], function (err, result) {
       if (err) {
