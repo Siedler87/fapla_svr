564390286ae0a8c0056bd33bf5342a3602316ab0
Siedler87 <112324174+Siedler87@users.noreply.github.com>
2022-10-05 23:47:07 +0200

    Update AddNewFamily.js

--- services/Familien/AddNewFamily.js
+++ services/Familien/AddNewFamily.js
@@ -12,7 +12,7 @@ async function create(AddNewFamily){
   });
   if (result.affectedRows) {
     var sql = 'SELECT `FamID` FROM `Familien` WHERE `Accountname` = ?';
-    const result = await db.query(sql, [AddNewFamily.accountname.trim()], function (err, result) {
+    const ergebnis = await db.query(sql, [AddNewFamily.accountname.trim()], function (err, result) {
       if (err) {
         let message = 'Error';
         return {message};
@@ -22,7 +22,7 @@ async function create(AddNewFamily){
     
 
       var sql = 'INSERT INTO FamilienAdmin(`FamID`, `BenID`) VALUES (?,?)';
-      const subresult = await db.query(sql, [result[0].FamID, AddNewFamily.benid.trim()], function (err, subresult) {
+      const subresult = await db.query(sql, [ergebnis[0].FamID, AddNewFamily.benid], function (err, subresult) {
         if (err) {
           let message = 'Error';
           return {message};
