04fc994abaaf02a1f582127fc438c9aebc63ff44
Siedler87 <112324174+Siedler87@users.noreply.github.com>
2022-10-16 14:51:42 +0200

    Update DeleteUser.js

--- services/Benutzer/DeleteUser.js
+++ services/Benutzer/DeleteUser.js
@@ -60,7 +60,7 @@ async function DeleteUser(DeleteUser){
   } 
   if (DeleteUser.benid != null && DeleteUser.aktion === 'nebenaccountloeschen'){
 
-    var sql = 'delete `BenutzerAltersstufen`, `BenutzerGruppen`, `BenutzerTermine`, `FamilienBenutzer`, `Hauptmenuerechte`, `FamilienAdmin` from `Benutzer` LEFT JOIN `BenutzerAltersstufen` ON `BenutzerAltersstufen`.`BenID`  = `Benutzer`.`BenID` LEFT JOIN `BenutzerGruppen` ON `BenutzerGruppen`.`BenID`  = `Benutzer`.`BenID` LEFT JOIN `BenutzerTermine` ON `BenutzerTermine`.`BenID`  = `Benutzer`.`BenID` LEFT JOIN `FamilienBenutzer` ON `FamilienBenutzer`.`BenID`  = `Benutzer`.`BenID` LEFT JOIN `Hauptmenuerechte` ON `Hauptmenuerechte`.`BenID`  = `Benutzer`.`BenID` LEFT JOIN `FamilienAdmin` ON `FamilienAdmin`.`BenID`  = `Benutzer`.`BenID` where `Benutzer`.`BenID` = ?';
+    var sql = 'delete `BenutzerAltersstufen`, `BenutzerGruppen`, `BenutzerTermine`, `FamilienBenutzer`, `Hauptmenuerechte`, `FamilienAdmin` from `Benutzer` LEFT JOIN `BenutzerAltersstufen` ON `BenutzerAltersstufen`.`BenID`  = `Benutzer`.`BenID` LEFT JOIN `BenutzerGruppen` ON `BenutzerGruppen`.`BenID`  = `Benutzer`.`BenID` LEFT JOIN `BenutzerTermine` ON `BenutzerTermine`.`BenID`  = `Benutzer`.`BenID` LEFT JOIN `FamilienBenutzer` ON `FamilienBenutzer`.`BenID`  = `Benutzer`.`BenID` LEFT JOIN `Hauptmenuerechte` ON `Hauptmenuerechte`.`BenID`  = `Benutzer`.`BenID` LEFT JOIN `FamilienAdmin` ON `FamilienAdmin`.`BenID`  = `Benutzer`.`BenID` where `Benutzer`.`BenID` = ? AND `Benutzer`.`Passwort` is null AND `Benutzer`.`Email` is null';
     const vorergebnis = await db.query(sql, [DeleteUser.benid], function (err, result) {
       if (err) {
         let message = 'Error';
