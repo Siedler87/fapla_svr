a51b27def60bf067f8f878b103bca78a8208fb70
Siedler87 <112324174+Siedler87@users.noreply.github.com>
2022-10-27 01:16:17 +0200

    Update GetAllDatesForUserFamily.js

--- services/Termine/GetAllDatesForUserFamily.js
+++ services/Termine/GetAllDatesForUserFamily.js
@@ -1,7 +1,7 @@
 const db = require('../db');
 
 async function GetAllDatesForUserFamily(GetAllDatesForUserFamily){
-
+  console.log(GetAllDatesForUserFamily);
   var sql = 'SELECT `BenutzerTermine`.`BenID`, `Termine`.`TerID`, `Termine`.`Titel`, `Termine`.`Beschreibung`, `Termine`.`Start`, `Termine`.`Ende`, `Termine`.`Erinnerung`, `Terminarten`.`Wiederholung`, `Terminarten`.`WdhAlleXTage`, `Terminarten`.`WdhAlleXWochen`,`Terminarten`.`WdhAlleXMonate`, `Verantwortlichkeiten`.`Kuerzel`, `Icons`.`Pfad` FROM `BenutzerTermine` LEFT JOIN `Verantwortlichkeiten` on `Verantwortlichkeiten`.`VerID` = `BenutzerTermine`.`VerID` LEFT JOIN `Termine` on `BenutzerTermine`.`TerID` = `Termine`.`TerID` LEFT JOIN `Icons` on `Termine`.`IconID` = `Icons`.`IconID` LEFT JOIN `Terminarten` on `Terminarten`.`TarID` = `Termine`.`TarID` where `BenutzerTermine`.`BenID` = ? ORDER BY `Termine`.`Start` ASC;';
   const ergebnis = await db.query(sql, [GetAllDatesForUserFamily.benid], function (err, result) {
     if (err) {
