71d1e005718587d71c935f4cb9636a52f9342279
Siedler87 <112324174+Siedler87@users.noreply.github.com>
2022-10-28 00:53:10 +0200

    Update GetAllDatesForUserFamily.js

--- services/Termine/GetAllDatesForUserFamily.js
+++ services/Termine/GetAllDatesForUserFamily.js
@@ -1,18 +1,31 @@
 const db = require('../db');
 
 async function GetAllDatesForUserFamily(GetAllDatesForUserFamily){
-  var sql = 'SELECT `BenutzerTermine`.`BenID`, `Termine`.`TerID`, `Termine`.`Titel`, `Termine`.`Beschreibung`, `Termine`.`Start`, `Termine`.`Ende`, `Termine`.`Erinnerung`, `Terminarten`.`Wiederholung`, `Terminarten`.`WdhAlleXTage`, `Terminarten`.`WdhAlleXWochen`,`Terminarten`.`WdhAlleXMonate`, `Verantwortlichkeiten`.`Kuerzel`, `Icons`.`Pfad` FROM `BenutzerTermine` LEFT JOIN `Verantwortlichkeiten` on `Verantwortlichkeiten`.`VerID` = `BenutzerTermine`.`VerID` LEFT JOIN `Termine` on `BenutzerTermine`.`TerID` = `Termine`.`TerID` LEFT JOIN `Icons` on `Termine`.`IconID` = `Icons`.`IconID` LEFT JOIN `Terminarten` on `Terminarten`.`TarID` = `Termine`.`TarID` where `BenutzerTermine`.`BenID` = ? ORDER BY `Termine`.`Start` ASC;';
-  const ergebnis = await db.query(sql, [GetAllDatesForUserFamily.benid], function (err, result) {
+  var sql = 'SELECT `Benutzer`.`BenID` FROM `Benutzer`  where `Benutzer`.`BenID` = ?';
+  const vorergebnis = await db.query(sql, [GetAllDatesForUserFamily.benid], function (err, result) {
     if (err) {
       let message = 'Error';
       return {message};
     };
   });
-  if (ergebnis.length < 1) {
-    let message = 'Account_not_found';
+  if (vorergebnis.length < 1) {
+    let message = 'Acc_not_found';
     return {message};
   } else {
-      return {ergebnis};
+    var sql = 'SELECT `BenutzerTermine`.`BenID`, `Termine`.`TerID`, `Termine`.`Titel`, `Termine`.`Beschreibung`, `Termine`.`Start`, `Termine`.`Ende`, `Termine`.`Erinnerung`, `Terminarten`.`Wiederholung`, `Terminarten`.`WdhAlleXTage`, `Terminarten`.`WdhAlleXWochen`,`Terminarten`.`WdhAlleXMonate`, `Verantwortlichkeiten`.`Kuerzel`, `Icons`.`Pfad` FROM `BenutzerTermine` LEFT JOIN `Verantwortlichkeiten` on `Verantwortlichkeiten`.`VerID` = `BenutzerTermine`.`VerID` LEFT JOIN `Termine` on `BenutzerTermine`.`TerID` = `Termine`.`TerID` LEFT JOIN `Icons` on `Termine`.`IconID` = `Icons`.`IconID` LEFT JOIN `Terminarten` on `Terminarten`.`TarID` = `Termine`.`TarID` where `BenutzerTermine`.`BenID` = ? ORDER BY `Termine`.`Start` ASC;';
+    const ergebnis = await db.query(sql, [GetAllDatesForUserFamily.benid], function (err, result) {
+      if (err) {
+        let message = 'Error';
+        return {message};
+      };
+    });
+
+      if (ergebnis.length < 1) {
+        let message = 'Date_not_found';
+        return {message};
+      } else {
+        return {ergebnis};
+      }
   }
 }
 
